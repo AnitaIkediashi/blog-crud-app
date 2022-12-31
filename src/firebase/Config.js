10c6e1af2b09731c7d63f02e9998c6f9ad987c8e
Anita Ikediashi <104375245+AnitaIkediashi@users.noreply.github.com>
2022-12-31 10:26:39 +0100

    added env variables

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -5,11 +5,11 @@ import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API,
-  authDomain: "blog-crud-app-eeeb9.firebaseapp.com",
-  projectId: "blog-crud-app-eeeb9",
-  storageBucket: "blog-crud-app-eeeb9.appspot.com",
-  messagingSenderId: "387271712336",
-  appId: "1:387271712336:web:e6eff883599326dc1e7b55",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
